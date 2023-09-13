3c0ef6c78c8bebd29f29e747f3d3fea1f08182fb
artem <140896029+nonfucius@users.noreply.github.com>
2023-09-13 23:06:09 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x >> y;
+    
+    int sum = x + y;
+
+    cout << sum << endl;
+}
+    
+
+
